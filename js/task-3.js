e7a08db0421a2ffde7033b35c3234947e4e0aa0e
Tetiana Baranova <syniovych.tania@gmail.com>
2024-10-08 16:00:42 +0300

    tasl-1-2-3-4

---
+++ js/task-3.js
@@ -0,0 +1,19 @@
+function checkForSpam(message) {
+    const blackListenWord1 = "spam";
+    const blackListenWord2 = "sale";
+    const normstr = message.toLowerCase();
+    if (normstr.includes(blackListenWord1) || normstr.includes(blackListenWord2))
+    {
+        return `true`;
+    } else {
+        return `false`;
+    }
+}
+    
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
